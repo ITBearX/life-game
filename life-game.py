7c39a5b62e64f38ae31a20613483168cd5e8ed5b
Denis Artemkin <d.artemkin@gmail.com>
2021-03-13 14:48:47 +0300

    Create signal processing functions stub

--- life-game.py
+++ life-game.py
@@ -10,6 +10,28 @@ class Handler:
     def __init__(self):
         self.window_is_hidden = False
 
+    def onNew(self, status):
+        status.set_text("Started a new grid")
+
+    def onRand(self, status):
+        status.set_text("Created a random grid")
+
+    def onLoad(self, status):
+        status.set_text("Unimplemented yet...")
+
+    def onSave(self, status):
+        status.set_text("Unimplemented yet...")
+
+    def onNext(self, status):
+        status.set_text("Next generation")
+
+    def onToggleAuto(self, status):
+        tb = builder.get_object('toggleAuto')
+        if tb.get_active():
+            status.set_text("Auto playing")
+        else:
+            status.set_text("Stopped")
+
     def onQuit(self, *args):
         Gtk.main_quit()
 
