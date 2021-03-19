7d89142c5eea6ba02de8e0e8fb0e7ad3a06d1600
Denis Artemkin <d.artemkin@gmail.com>
2021-03-20 01:44:03 +0300

    Implement autoplay.

--- life_main.py
+++ life_main.py
@@ -2,9 +2,11 @@
 
 import signal
 import time
+from threading import Timer
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+
 from life_cairo import LifeGridCairo
 
 
@@ -12,11 +14,11 @@ class Handler:
 
     def onNew(self, *args):
         life.zeros()
-        self.update_drawing('Created an empty grid')
+        update_drawing('Created an empty grid')
 
     def onRand(self, *args):
         life.rand(controls['alive_perc'].get_value()/100)
-        self.update_drawing('Created a random grid')
+        update_drawing('Created a random grid')
 
     def onLoad(self, *args):
         status.set_text('Unimplemented yet...')
@@ -24,18 +26,8 @@ class Handler:
     def onSave(self, *args):
         status.set_text('Unimplemented yet...')
 
-    def onNext(self, *args):
-        life.next_gen()
-        self.update_drawing('Next generation')
-
-    def onToggleAuto(self, *args):
-        tb = builder.get_object('toggleAuto')
-        if tb.get_active():
-            t = time.Timer(1.0, autoplay)
-            t.start()
-            status.set_text('Auto playing')
-        else:
-            status.set_text('Stopped')
+    def onPlay(self, *args):
+        play()
 
     def onDraw(self, area, context):
         width = area.get_allocated_width()
@@ -45,8 +37,8 @@ class Handler:
     def onGridClick(self, area, event):
         flip_result = life.flip_on_click(event.x, event.y)
         if flip_result is not None:
-            self.update_drawing('Cell {},{} flipped'.format(flip_result[0],
-                                                            flip_result[1]))
+            update_drawing('Cell {},{} flipped'.format(flip_result[0],
+                                                       flip_result[1]))
 
     def onShapeChange(self, *args):
         rows = controls['rows'].get_value()
@@ -55,7 +47,7 @@ class Handler:
 
         life.h_wrap = controls['h_wrap'].get_active()
         life.v_wrap = controls['v_wrap'].get_active()
-        self.update_drawing('Grid shape changed')
+        update_drawing('Grid shape changed')
 
     def onRulesChange(self, *args):
         if controls['moore'].get_active():
@@ -85,9 +77,22 @@ class Handler:
     def onQuit(self, *args):
         Gtk.main_quit()
 
-    def update_drawing(self, msg=''):
-        status.set_text(msg)
-        drawing_area.queue_draw()
+
+def play():
+    life.next_gen()
+    drawing_area.queue_draw()
+
+    if auto_button.get_active():
+        t = Timer(1 / controls['speed'].get_value(), play)
+        t.start()
+        status.set_text('Auto playing')
+    else:
+        status.set_text('Idle')
+
+
+def update_drawing(msg=''):
+    status.set_text(msg)
+    drawing_area.queue_draw()
 
 
 if __name__ == '__main__':
@@ -102,11 +107,13 @@ if __name__ == '__main__':
     window = builder.get_object('window_main')
     drawing_area = builder.get_object('drawing_area')
     status = builder.get_object('lb_status')
+    auto_button = builder.get_object('bt_auto')
 
     ctrls_list = ['rows', 'cols', 'h_wrap', 'v_wrap', 'alive_perc',
                   'alive_nbr_min', 'alive_nbr_max',
                   'birth_nbr_min', 'birth_nbr_max',
-                  'moore', 'von_neumann']
+                  'moore', 'von_neumann',
+                  'speed']
     controls = {}
     for ctrl_name in ctrls_list:
         controls[ctrl_name] = builder.get_object('ctrl_' + ctrl_name)
